4490650a8c5136a0f30f3ba7978c2a5162abe13e
Unioscar40 <odtg1@gcloud.ua.es>
2023-07-09 13:10:02 +0200

    implement dot Product sse

--- lib/bowii/utils.cpp
+++
@@ -1,5 +0,0 @@
-#include "utils.h"
-
-void* algined_memorySSE(size_t alignment, size_t size) {
-    return aligned_alloc(alignment, size);
-}
--- lib/bowii/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdlib.h>
-
-
-void* aligned_memorySSE(size_t alignment, size_t size);
